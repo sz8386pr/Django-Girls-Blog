8a1763ba8666b121f9a4cbe92295c58f808e12d4
sz8386pr <sz8386pr>
2018-03-22 14:42:40 -0500

    login feature

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -17,13 +18,13 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 # posting new form.
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():     # if valid. Currently, as long as the title & test fields are not empty
             post = form.save(commit=False)  # don't save it yet
             post.author = request.user  # set the author as user
-            post.published_date = timezone.now()    # set the published_date as now
             post.save()     # and THEN save with the title & text from the form
             return redirect('post_detail', pk=post.pk)  # redirect to post detail page that's just been posted
     else: # by default, display an empty form
@@ -31,6 +32,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 # edit an existing post accessed from the post detail view
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -38,9 +40,31 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+# lists of UNpublished posts
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+# publish the post
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+# delete the post
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STSTIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
